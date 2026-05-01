2c0ea7245f5290b1d905dae626485ddb0289b2b5
MaddoxBlueNinja <Maddoxblueninja@gmail.com>
2026-04-30 19:58:44 -0500

    Made model prefabs and car movement

--- Gravity Gauge/Assets/_Scripts/CarMovement.cs
+++ Gravity Gauge/Assets/_Scripts/CarMovement.cs
@@ -1,8 +1,18 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CarMovement : MonoBehaviour
 {
-    public float speed;
+    public float speedZ;
+    public float accelZ;
+    public float decelZ;
+    int accelZAim;
+
+    float speedX;
+    public float accelX;
+    public float decelX;
+    int accelXAim;
+
     public float gravityCD;
     public float health;
     public float minSpeed;
@@ -13,16 +23,77 @@ public class CarMovement : MonoBehaviour
 
     void Update()
     {
-        
+        AccelInput();
     }
 
     void FixedUpdate()
     {
-
+        SpeedUpdate();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Road") return;
+        // Put lose conditions here.
+    }
+
+    void AccelInput()
+    {
+        if (Keyboard.current.wKey.isPressed)
+        {
+            accelZAim = 1;
+        }
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            accelZAim = -1;
+        }
+        else
+        {
+            accelZAim = 0;
+        }
+
+        if (Keyboard.current.dKey.isPressed)
+        {
+            accelXAim = 1;
+        }
+        else if (Keyboard.current.aKey.isPressed)
+        {
+            accelXAim = -1;
+        }
+        else
+        {
+            accelXAim = 0;
+        }
+    }
+
+    void SpeedUpdate()
+    {
+        if (accelZAim != 0)
+        {
+            speedZ += accelZ * accelZAim;
+        }
+        else
+        {
+            speedZ *= 1 - decelZ;
+        }
+
+        if (accelXAim != 0)
+        {
+            if (Mathf.Sign(accelXAim) != Mathf.Sign(speedX))
+            {
+                speedX *= 1 - decelX;
+            }
+
+            speedX += accelX * accelXAim;
+        }
+        else
+        {
+            speedX *= 1 - decelX;
+        }
+
+        Vector3 pos = this.transform.position;
+        pos.z += speedZ;
+        pos.x += speedX;
+        this.transform.position = pos;
     }
 }
